a75ce72f58264e7dc389327af80e45aadbd00c09
Brad Bartram <brad@darksbian.com>
2022-01-07 12:06:00 -0500

    simplified the run_server to avoid some errors

--- main.py
+++ main.py
@@ -365,4 +365,5 @@ def dashboard_summary_numbers(report):
 # Server Run
 ###################################################
 if __name__ == "__main__":
-    app.run_server(debug=False, host="0.0.0.0", port=8050, dev_tools_hot_reload=True)
+    # app.run_server(debug=False, host="0.0.0.0", port=8050, dev_tools_hot_reload=True)
+    app.run_server(debug=True)
